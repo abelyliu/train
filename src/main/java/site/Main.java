b6ff67376722c761683df7404d179df3e5d8183e
abely <qingsong.liu@crgecent.com>
2019-04-27 16:16:03 +0800

    init

--- src/main/java/site/Main.java
+++ src/main/java/site/Main.java
@@ -7,34 +7,85 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
+
+    public static  Map<String, String> codeMap;
     public static void main(String[] args) throws IOException {
         String startLocation = "阜阳";
         String endLocation = "杭州";
-        String date = "2019-04-28";
-        Map<String, String> codeMap = getCodeMap();
-//        getTicket(date, codeMap.get(startLocation), codeMap.get(endLocation));
+        String date = "2019-05-04";
+        codeMap = getCodeMap();
+        getTicket(date, startLocation, endLocation);
 
     }
 
-    private void getTrainNo(String trainNo, String startLocation, String endLocation, String date) throws IOException {
-        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=" + trainNo + "&from_station_telecode=" + startLocation + "&to_station_telecode=" + endLocation + "&depart_date=" + date).execute();
+    private static void getTrainNo(String trainNo, String ls,String le, String date) throws IOException {
+        String startLocationCode = codeMap.get(ls);
+        String endLocationCode= codeMap.get(le);
+
+        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=" + trainNo + "&from_station_telecode=" + startLocationCode + "&to_station_telecode=" + endLocationCode + "&depart_date=" + date).ignoreContentType(true).execute();
         String body = execute.body();
         Map<String,String> map = new HashMap<>();
         JSONArray jsonArray = JSONObject.parseObject(body).getJSONObject("data").getJSONArray("data");
-        for (int i = 0; i < jsonArray.size() - 1; i++) {
-            if (i == jsonArray.size() - 2) {
-
-            }
+        Integer start = null;
+        Integer end = null;
+        for (int i = 0; i < jsonArray.size(); i++) {
             JSONObject cur = (JSONObject) jsonArray.get(i);
-            JSONObject next = (JSONObject) jsonArray.get(i + 1);
+            String stationName = cur.getString("station_name");
+            if (stationName.contains(ls)) {
+                start = cur.getInteger("station_no")-1;
+            } else if (stationName.contains(le)) {
+                end = cur.getInteger("station_no")-1;
+            }
+        }
+        List<String> startLocations = new ArrayList<>();
+        List<String> endLocations = new ArrayList<>();
+        for (int i = start; i >= 0; i--) {
+            JSONObject cur = (JSONObject) jsonArray.get(i);
+            String stationName = cur.getString("station_name");
+            startLocations.add(codeMap.get(stationName));
+        }
+
+        for (int i = end; i < jsonArray.size(); i++) {
+            JSONObject cur = (JSONObject) jsonArray.get(i);
+            String stationName = cur.getString("station_name");
+            endLocations.add(codeMap.get(stationName));
+        }
+
+        for (String s : startLocations) {
+            for (String e : endLocations) {
+                getTicket2(date,s,e);
+            }
         }
     }
 
-    private static void getTicket(String date, String startLocation, String endLocation) throws IOException {
+    private static void getTicket(String date,String ls,String le) throws IOException {
+        String startLocationCode= codeMap.get(ls);
+        String endLocationCode = codeMap.get(le);
+        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=" + date + "&leftTicketDTO.from_station=" + startLocationCode + "&leftTicketDTO.to_station=" + endLocationCode + "&purpose_codes=ADULT").ignoreContentType(true).execute();
+        JSONObject jsonObject = JSONObject.parseObject(execute.body());
+        JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("result");
+        Map<String,String> map = new HashMap<>();
+        for (Object o : jsonArray) {
+            String str = (String) o;
+            String[] split = str.split("\\|");
+            if (StringUtils.isNotBlank(split[28]) && !"无".equals(split[28])) {
+                System.out.println(split[3]+":"+date+":"+ls+":"+le);
+                break;
+            }
+        }
+        String s = jsonArray.getString(jsonArray.size() - 1);
+        String[] split = s.split("\\|");
+        getTrainNo(split[2], ls, le, date);
+
+    }
+    private static void getTicket2(String date, String startLocation, String endLocation) throws IOException {
         Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=" + date + "&leftTicketDTO.from_station=" + startLocation + "&leftTicketDTO.to_station=" + endLocation + "&purpose_codes=ADULT").ignoreContentType(true).execute();
         JSONObject jsonObject = JSONObject.parseObject(execute.body());
         JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("result");
@@ -43,7 +94,8 @@ public class Main {
             String str = (String) o;
             String[] split = str.split("\\|");
             if (StringUtils.isNotBlank(split[28]) && !"无".equals(split[28])) {
-                System.out.println(split[3]);
+                System.out.println(split[3]+":"+date+":"+startLocation+":"+endLocation);
+                break;
             }
         }
     }
