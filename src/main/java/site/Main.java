712f65eadb4070a49fe10eaa4141d7396ae90d13
刘青松 <qingsong.liu@crgecent.com>
2019-09-19 20:19:27 +0800

    代码调整

--- src/main/java/site/Main.java
+++ src/main/java/site/Main.java
@@ -11,7 +11,6 @@ import org.jsoup.Jsoup;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,17 +24,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String startLocation = "阜阳";
         String endLocation = "杭州";
-        String date = "2019-05-04";
+        String date = "2019-10-06";
         getCodeMap();
         getTicket(date, startLocation, endLocation);
+//        Map<String, String> headerMap = getHeaderMap();
+//        System.out.println(headerMap);
 
     }
 
     private static void getTrainNo(String trainNo, String ls, String le, String date) throws IOException {
         String startLocationCode = codeMap.get(ls);
         String endLocationCode = codeMap.get(le);
-
-        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=" + trainNo + "&from_station_telecode=" + startLocationCode + "&to_station_telecode=" + endLocationCode + "&depart_date=" + date).ignoreContentType(true).execute();
+//                                                      https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=5e000K143820&from_station_telecode=HZH&to_station_telecode=FYH&depart_date=2019-09-19
+        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=" + trainNo + "&from_station_telecode=" + startLocationCode + "&to_station_telecode=" + endLocationCode + "&depart_date=" + date)
+                                           .headers(getHeaderMap()).ignoreContentType(true).execute();
         String body = execute.body();
         JSONArray jsonArray = JSONObject.parseObject(body).getJSONObject("data").getJSONArray("data");
         Integer start = null;
@@ -73,7 +75,8 @@ public class Main {
     private static void getTicket(String date, String ls, String le) throws IOException {
         String startLocationCode = codeMap.get(ls);
         String endLocationCode = codeMap.get(le);
-        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=" + date + "&leftTicketDTO.from_station=" + startLocationCode + "&leftTicketDTO.to_station=" + endLocationCode + "&purpose_codes=ADULT").ignoreContentType(true).execute();
+        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=" + date + "&leftTicketDTO.from_station=" + startLocationCode + "&leftTicketDTO.to_station=" + endLocationCode + "&purpose_codes=ADULT").ignoreContentType(true)
+                                          .headers(getHeaderMap()).execute();
         JSONObject jsonObject = JSONObject.parseObject(execute.body());
         JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("result");
         Map<String, String> map = new HashMap<>();
@@ -86,14 +89,15 @@ public class Main {
 //            }
         }
         //todo change
-        String s = jsonArray.getString(jsonArray.size() - 9);
+        String s = jsonArray.getString(jsonArray.size()-9);
         String[] split = s.split("\\|");
         getTrainNo(split[2], ls, le, date);
 
     }
 
     private static void getTicket2(String date, String startLocation, String endLocation, String trainNo) throws IOException {
-        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=" + date + "&leftTicketDTO.from_station=" + startLocation + "&leftTicketDTO.to_station=" + endLocation + "&purpose_codes=ADULT").ignoreContentType(true).execute();
+        Connection.Response execute = Jsoup.connect("https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=" + date + "&leftTicketDTO.from_station=" + startLocation + "&leftTicketDTO.to_station=" + endLocation + "&purpose_codes=ADULT")
+                                           .headers(getHeaderMap()).ignoreContentType(true).execute();
         JSONObject jsonObject = JSONObject.parseObject(execute.body());
         JSONArray jsonArray = jsonObject.getJSONObject("data").getJSONArray("result");
         Map<String, String> map = new HashMap<>();
@@ -101,6 +105,13 @@ public class Main {
             String str = (String) o;
             String[] split = str.split("\\|");
             if (StringUtils.isNotBlank(split[28]) && !"无".equals(split[28]) && Objects.equals(trainNo, split[2])) {
+                System.out.println("卧铺");
+                System.out.println(split[3] + ":" + date + ":" + codeMap.inverse().get(startLocation) + ":" + codeMap.inverse().get(endLocation));
+                break;
+            }
+
+            if (StringUtils.isNotBlank(split[29]) && !"无".equals(split[29]) && Objects.equals(trainNo, split[2])) {
+                System.out.println("硬座");
                 System.out.println(split[3] + ":" + date + ":" + codeMap.inverse().get(startLocation) + ":" + codeMap.inverse().get(endLocation));
                 break;
             }
@@ -132,6 +143,29 @@ public class Main {
             }
         }
         return codeMap;
+    }
+
+    private static Map<String,String> getHeaderMap() {
+        String s = "Host: kyfw.12306.cn\n" +
+                "Connection: keep-alive\n" +
+                "Cache-Control: no-cache\n" +
+                "Accept: */*\n" +
+                "X-Requested-With: XMLHttpRequest\n" +
+                "If-Modified-Since: 0\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36\n" +
+                "Sec-Fetch-Mode: cors\n" +
+                "Sec-Fetch-Site: same-origin\n" +
+                "Referer: https://kyfw.12306.cn/otn/leftTicket/init?linktypeid=dc&fs=%E6%9D%AD%E5%B7%9E,HZH&ts=%E9%98%9C%E9%98%B3,FYH&date=2019-09-19&flag=N,N,Y\n" +
+                "Accept-Encoding: gzip, deflate, br\n" +
+                "Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,th;q=0.7,id;q=0.6,zh-TW;q=0.5\n" +
+                "Cookie: JSESSIONID=49E4B79A6AEECBF623C84B13AE36DF08; _jc_save_wfdc_flag=dc; RAIL_EXPIRATION=1569141643996; RAIL_DEVICEID=LL9NUsGAyPyqaiktu_Cg-TM9PSmjj8YUYqrC_sFkZ2Ab4Qrw1aZ2a7k6Aq7V7FbZXHVhSe75ae0FwoZhUaE9TDlfq1snfb4oLGPRHLcFIAlVqXcfr3_eOJnDKBlKE54VwyyNOcqOIfslBZarEykkawlzrKjXbpPW; _jc_save_toStation=%u961C%u9633%2CFYH; BIGipServerpool_passport=183304714.50215.0000; route=6f50b51faa11b987e576cdb301e545c4; BIGipServerotn=535822858.64545.0000; _jc_save_fromStation=%u676D%u5DDE%2CHZH; _jc_save_fromDate=2019-09-19; _jc_save_toDate=2019-09-19";
+        String[] split = s.split("\n");
+        Map<String,String> map = new HashMap<>();
+        for (String s1 : split) {
+            String[] split1 = s1.split(": ");
+            map.put(split1[0],split1[1]);
+        }
+        return map;
     }
 
 //    private static Ticket
